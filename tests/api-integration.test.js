7bdf2424ac4a71d7c98e00a336bc5fa156289ab6
walaqi <walaqi@sohu.com>
2026-05-25 14:20:34 +0800

    change the endpoint to be  localhost

--- tests/api-integration.test.js
+++ tests/api-integration.test.js
@@ -15,7 +15,7 @@ import { fetch } from 'undici';
  */
 
 // Test server configuration
-const TEST_SERVER_BASE_URL = 'http://192.168.1.232:3000';
+const TEST_SERVER_BASE_URL = 'http://127.0.0.1:3010';
 const TEST_API_KEY = '123456'; // You may need to adjust this based on your server config
 const MODEL_PROVIDER = {
     // Model provider constants
